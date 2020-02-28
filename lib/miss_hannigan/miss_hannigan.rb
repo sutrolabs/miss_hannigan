245f4bb4e994fc35f4a38bf6eb2ff0e25321cf1f
Bradley Buda <bradleybuda@gmail.com>
2020-02-28 10:02:23 -0800

    Remove trailing spaces

--- lib/miss_hannigan/miss_hannigan.rb
+++ lib/miss_hannigan/miss_hannigan.rb
@@ -3,7 +3,7 @@ module MissHannigan
 
   module ClassMethods
 
-    def has_many(name, scope = nil, **options, &extension) 
+    def has_many(name, scope = nil, **options, &extension)
       nullify_then_purge = false
 
       # we're really just relying on :nullify. so just return our dependent option to that
@@ -20,13 +20,13 @@ module MissHannigan
         # has the details of the relation to Child
         reflection_details = reflection[name.to_s]
 
-        # I bet folks are going to forget to do the migration of foreign_keys to accept null. Rails defaults 
+        # I bet folks are going to forget to do the migration of foreign_keys to accept null. Rails defaults
         # to not allow null.
         if !reflection_details.klass.columns.find { |c| c.name == reflection_details.foreign_key }.null
-          raise "The foreign key must be nullable to support MissHannigan. You should create a migration to: 
+          raise "The foreign key must be nullable to support MissHannigan. You should create a migration to:
             change_column_null :#{name.to_s}, :#{reflection_details.foreign_key}, true"
         end
-        
+
         after_destroy do |this_object|
           CleanupJob.perform_later(reflection_details.klass.to_s, reflection_details.foreign_key)
         end
@@ -38,7 +38,7 @@ module MissHannigan
 
   class CleanupJob < ActiveJob::Base
     queue_as :default
-    
+
     def perform(klass_string, parent_foreign_key)
       klass = klass_string.constantize
 
